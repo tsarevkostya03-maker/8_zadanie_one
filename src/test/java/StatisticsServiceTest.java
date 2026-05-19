171edf319b44845712886da85fbce4bb45788e68
tsarevkostya03-maker <tsarevkostya03@gmail.com>
2026-05-19 22:53:03 +0300

    Update StatisticsServiceTest.java

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -4,10 +4,22 @@ import org.junit.jupiter.api.Test;
 
 public class StatisticsServiceTest {
     @Test
-    void findMax() {
+    void findMaxWhenMaxIsInMiddle() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {5, 12, 8, 4, 5, 3, 8, 6, 11, 11, 10};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWhenMaxIsFirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 10};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
